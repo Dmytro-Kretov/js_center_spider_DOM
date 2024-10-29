f3800f632fb970cdb0486f1151c7f86ce22dfe74
Dmytro Kretov <dmytro.kretov@gmail.com>
2024-10-29 14:27:17 +0200

    make build

---
+++ index.f21648f4.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider"),o=t.getBoundingClientRect(),i=e.getBoundingClientRect(),l=(o.height-i.height)/2,n=(o.width-i.width)/2;e.style.position="absolute",e.style.top="".concat(l,"px"),e.style.left="".concat(n,"px");
+//# sourceMappingURL=index.f21648f4.js.map
